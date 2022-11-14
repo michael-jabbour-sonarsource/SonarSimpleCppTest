1979dc6e1134a12f88dd8cfca9fd0bb27a9493f6
Michael Jabbour <michael.jabbour@sonarsource.com>
2022-11-14 08:29:09 +0100

    Change gcov arguments

--- generate_coverage_reports.sh
+++ generate_coverage_reports.sh
@@ -10,5 +10,5 @@ ctest
 
 mkdir gcov_dir
 pushd gcov_dir
-find .. -name '*.gcda' -print0 | xargs -0 -L1 gcov -a -b -c -f -u -p -l
+find .. -name '*.gcda' -print0 | xargs -0 -L1 gcov -a -b -p -l
 popd
