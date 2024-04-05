a21476ccd8f890d608d0e510e62ee7aedfdf8362
Michael Jabbour <117195239+michael-jabbour-sonarsource@users.noreply.github.com>
2024-04-05 17:56:36 +0200

    Some code

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,15 @@
 #include "clamped_add.h"
+#include <iostream>
+
+
+void f() {
+    int x = 0;
+    #include "someheader.h";
+}
+
 
 int main() {
+    f();
     // just a dummy executable for now
     return clamped_add(0u, 0u, 10u);
 }
